ef1d4316d54082edc2607d43fe375595b4211e75
Melnikov Ilya <mid16042001@gmail.ru>
2021-10-27 21:00:08 +0300

    start

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		Console.WriteLine("Здравствуйте! Это программа по поиску максимального из 3-х чисел.");
+
+		Console.Write("Введите первое число: ");
+		float a = Convert.ToSingle(Console.ReadLine());
+
+		Console.Write("Введите второе число: ");
+		float b = Convert.ToSingle(Console.ReadLine());
+
+		Console.Write("Введите третье число: ");
+		float c = Convert.ToSingle(Console.ReadLine());
+
+		if (a > b < c)
+        {
+			Console.WriteLine("OK");
+        }
+	}
+}
---
+++ Script_day_of_week/Script_day_of_week/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Script_day_of_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
