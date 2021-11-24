94a2b4bb6564bf851a5e9c7b5614ff586aefefde
Melnikov Ilya <mid16042001@gmail.ru>
2021-11-24 22:48:17 +0300

    добавлен файл со скриптами по заданиям в классе по теме "строки" за 18.11.21

---
+++ String_on_class_18_11_21.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 3. Является ли слово полиндромом?
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine();
+            bool isPolindrom = true;
+
+            for (int i = 0; i < (word.Length / 2); i++)
+            {
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    isPolindrom = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Утверждение, что слово '{word}' является полиндромом - {isPolindrom}");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+////18.11 Задание 1. Реверсивная строка
+//Console.Write("Оригинальная строка: ");
+//string str = Console.ReadLine();
+//string reverce_str = "";
+
+//for (int i = str.Length - 1; i >= 0; i--)
+//{
+//    reverce_str += str[i];
+//}
+//Console.WriteLine($"Перевёрнутая строка: {reverce_str}");
+
+
+
+////Задание 2. Капитализация текста - изменение первых букв в словах строки на заглавные
+//Console.Write("Введите текст: ");
+//string[] _words = Console.ReadLine().Split(' ');
+//string result = "";
+//for (int i = 0; i < _words.Length; i++)
+//{
+//    result += char.ToUpper(_words[i][0]);
+//    for (int j = 1; j < _words[i].Length; j++)
+//    {
+//        result += _words[i][j];
+//    }
+//    result += ' ';
+//}
+//Console.WriteLine(result);
---
+++ Test1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_r
+{
+    class Program
+    {
+        // Враиант 2
+        static void Main(string[] args)         
+        {
+            //Задание номер 3
+            //Дана последовательность их n целых чисел. Первое число в последовательности чётное.
+            //Найти сумму всех идущих подряд в начале последовательности чётных чисел. Условный оператор не использовать. 
+            Console.Write("Введите последовательность чисел (первое число дожно быть чётным): ");
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            int i = 0;
+            int sum = 0;
+
+            do
+            {
+                sum += array[i];
+                i++;
+            } while (array[i] % 2 == 0);
+
+            Console.WriteLine($"Сумма всех идущих подряд в начале последовательности чётных чисел равна {sum}");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+////Задание номер 1
+////Определить, существует ли прямоугольный треугольник со сторонами x,y,z. Если - да,вычислить его площадь.
+
+//Console.Write("Введите длину 1-ой стороны треугольника: ");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Введите длину 2-ой стороны треугольника: ");
+//double y = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Введите длину 3-ей стороны треугольника: ");
+//double z = Convert.ToDouble(Console.ReadLine());
+
+//if ((x + y > z) && (x + z > y) && (y + z > x))
+//{
+//    double p = (x + y + z) / 2;
+//    double s = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+//    Console.WriteLine($"Треугольник существует. Площадь треугольника равна {s}");
+//}
+//else
+//{
+//    Console.WriteLine("Треугольника с заданными сторонами не существует.");
+//}
+
+
+
+//Задание номер 2
+////Даны два числа А и В (А<В). Найти сумму квадратов всех целых чисел от А до В включительно.
+//Console.Write("Введите число A: ");
+//int A = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите число B: ");
+//int B = Convert.ToInt32(Console.ReadLine());
+
+//int sum = 0;
+//for (int i = A; i <= B; i++)
+//{
+//    sum += i * i;
+//}
+//Console.WriteLine($"Сумма квадратов всех целых чисел от A до B: {sum}");
